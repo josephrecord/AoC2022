5c1859a5518fcf48cf71f7471063b17e02b855c7
josephrecord <joseph.record@gmail.com>
2022-12-03 23:55:03 -0500

    Update aoc22d3.py

--- aoc22d3.py
+++ aoc22d3.py
@@ -5,6 +5,7 @@ d = "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn"
 e = "ttgJtRGJQctTZtZT"
 f = "CrZsJsPPZsGzwwsLwLmpwMDw"
 
+
 def find_shared_items(rucksack: str) -> str:
     """Split the rucksack's contents into left and right compartments"""
     midpoint = len(rucksack) // 2
@@ -22,18 +23,41 @@ def priority(item: str) -> int:
         return ord(item.swapcase()) - 64
 
 
-print(find_shared_items(a))
-print(find_shared_items(b))
-print(find_shared_items(e))
-print(find_shared_items(d))
-print(find_shared_items(e))
-print(find_shared_items(f))
+# print(find_shared_items(a))
+# print(find_shared_items(b))
+# print(find_shared_items(e))
+# print(find_shared_items(d))
+# print(find_shared_items(e))
+# print(find_shared_items(f))
 
-total_priority = 0 
 
-with open("input3.txt") as f:
-    for line in f:
-        rucksack = line.strip()
+def solve1(rucksacks: list[str]) -> int:
+    total_priority = 0
+    for rucksack in rucksacks:
         total_priority += priority(find_shared_items(rucksack))
+    return total_priority
 
-total_priority
+def solve2(rucksacks: list[str]) -> int:
+    total_priority = 0
+    it = iter(rucksacks)
+    for x in it:
+        ruck1 = set(x)
+        ruck2 = set(next(it))
+        ruck3 = set(next(it))
+        item = ruck1.intersection(ruck2, ruck3).pop()
+        total_priority += priority(item)
+    return total_priority
+
+
+def main() -> None:
+    with open("input3.txt") as f:
+        parsed_input = [x.strip() for x in f.readlines()]
+
+    ans1 = solve1(parsed_input)
+    ans2 = solve2(parsed_input)
+    print(ans1, ans2)
+
+
+if __name__ == "__main__":
+    main()
+
