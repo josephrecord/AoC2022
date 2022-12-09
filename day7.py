9f09861c651b02717b78ee3d9270a9329128a3ee
josephrecord <joseph.record@gmail.com>
2022-12-08 22:51:10 -0500

    almost working...

--- day7.py
+++ day7.py
@@ -1,56 +1,49 @@
 from collections import defaultdict
 
-class Tree:
-    def __init__(self) -> None:
-        self.nodes = None
-    
-    def prune(self, dir_name: str):
-        pass
-
-
-class Node:
-    def __init__(self, name) -> None:
-        self.name = name
-        self.children = []
-        self.parent = None
-
 
 cmd_output = []
 dirs = defaultdict(list)
-direct_size = defaultdict(int)
+size = defaultdict(int)
 depths = defaultdict(set)
-parents = {}
+parent = {} # key's parent is value
 dirset = set()
+filesize = 0
 
-with open("input7test.txt") as f:
+with open("input7.txt") as f:
     for line in f:
         line = line.strip()
+        # print(f"raw: {line}")
         if line.startswith("$"):
-            # Command
             if cmd_output:
                 # print(f"ls: {cmd_output}")
                 pass
             cmd_output = [] # New command, clear previous output
+            # Command
             _, cmd, *args = line.split()
             if cmd == "cd":
                 arg = args[0]
                 # print(f"{cmd} {arg}")
                 if arg == "/":
-                    dirset.add(arg)
-                    stack = ["/"]
                     depth = 0
-                    parents[stack[-1]] = None
+                    # print("cd /")
+                    dir = (0, '/')
+                    stack = [dir]
+                    dirset.add(dir)
+                    parent[dir] = None
                 elif arg == "..":
+                    depth -= 1
+                    # print("cd ..")
                     # move up to parent dir
                     stack.pop()
-                    depth -= 1
                 else:
                     # move down to child dir
-                    dirset.add(arg)
-                    parents[arg] = stack[-1] # we learn info on current dir's parent only when we go down a level
-                    stack.append(arg)
                     depth += 1
-                # print(f"Current dir: {stack[-1]} \t Depth: {depth}")
+                    # print(f"cd {arg}")
+                    dir = (depth, arg)
+                    parent[dir] = stack[-1] # we learn info on current dir's parent only when we go down a level
+                    stack.append(dir)
+                    dirset.add(dir)
+                # print(f"Current dir: {stack[-1]}")
                 depths[depth].add(stack[-1])
             else:
                 # Only two commands so will print ls
@@ -61,23 +54,49 @@ with open("input7test.txt") as f:
             cmd_output.append(line.split())
             field1, field2 = line.split()
             if field1 == "dir":
-                dirs[stack[-1]].append(field2)
+                dirs[stack[-1]].append((depth + 1, field2))
             else:
-                direct_size[stack[-1]] += int(field1)
+                filesize += int(field1)
+                size[stack[-1]] += int(field1)
 
 
 # `direct_size` contains directory names as keys and the sum of the size
 # of the files in that immediate directory (but no info about sub-dirs).
 # If the immediate file size of the directory is > 100000, we can ignore it.
-small_dirs = {key: val for key, val in direct_size.items() if val <= 100000}
+# small_dirs = {key: val for key, val in direct_size.items() if val <= 100000}
 
 ans = 0
+threshold = 100000
 
-sizes = {}
+# for depth in reversed(depths.keys()):
+#     print(f"depth: {depth}")
+#     for dir in depths[depth]:
+#         dirsize = size[dir]
+#         parent = parents[dir]
+#         parent_size = size[parent]
+#         print(f"folder {dir} size: {dirsize}   parent {parent} size: {parent_size}")
+#         if dirsize > threshold:
+#             # any parent dir that contains `folder` can be ignored
+#             print(f"ignore {dir}")
+#         else:
+#             print(f"include {dir}")
+#             print(f"total is {ans} + {dirsize} = {ans+dirsize}")
+#             ans += dirsize
+#         # treat the folder as a file and add it to its parents' direct size
+#         size[parent] += dirsize 
+#         print(f"parent {parent} new size: {size[parent]}")
 
-full_depth = len(dirs['/'])
+s1 = size
 
-# print(dirset)
+for dir in list(sorted(dirset, reverse=True)):
+    if size[dir] > threshold:
+        print(f"{dir}:  {size[dir]} > {threshold} --> ignore")
+    else:
+        print(f"{dir}:  {size[dir]} <= {threshold} --> include")
+        print(f"total is {ans} + {size[dir]} = {ans+size[dir]}")
+        ans += size[dir]
+    print(f"add to parent {parent[dir]}: {size[parent[dir]]} + {size[dir]} = {size[parent[dir]] + size[dir]}")
+    size[parent[dir]] += size[dir]
 
 # Go to all leaves of tree
 # Since leaf has no subdirs, leaf size = direct size
@@ -87,11 +106,11 @@ full_depth = len(dirs['/'])
 
 # print(depths[full_depth])
 
-for d in dirset:
-    print(d)
-    while parents[d]:
-        d = parents[d]
-        print(d)
+# for d in dirset:
+#     print(d)
+#     while parents[d]:
+#         d = parents[d]
+#         print(d)
 
 # print(parents)
 
