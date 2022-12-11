ccadda32f93b7105a0ff83dfcdb81225c6f77ef2
josephrecord <joseph.record@gmail.com>
2022-12-10 20:36:43 -0500

    soln for day 7 part 1

--- day7.py
+++ day7.py
@@ -3,117 +3,101 @@ from dataclasses import dataclass
 from typing import Union
 
 
-@dataclass(frozen=True):
+@dataclass(frozen=True, order=True)
 class Dir:
     depth: int
     name: str
     parent: Union[None, "Dir"]
 
+
+@dataclass(frozen=True, order=True)
+class File:
+    size: int
+    name: str
+    directory: Dir
+
 root = Dir(0, '/', None)
-current_dir = root
 
 size = defaultdict(int)
+files = defaultdict(set)
 ls = defaultdict(int)    # num times ls run in dir (to prevent overcounting)
-parent = {}
+ls2 = defaultdict(int)    # num times ls run in dir (to prevent overcounting)
+dirset = set()
+fileset = set()
+
+dirset2 = set()
+
 
 with open("input7.txt") as f:
-    for line in f:
-        line = line.strip()
-        if line.startswith("$"):
-            # Command
-            _, cmd, *args = line.split()
-            if cmd == "cd":
-                arg = args[0]
-                if arg == "/":
-                    depth = 0
-                    stack = root
-                    current_dir = (depth, stack[-1])
-                elif arg == "..":
-                    depth -= 1
-                    stack.pop()
-                    current_dir = (depth, stack[-1])
-                else:
-                    p = current_dir
-                    depth += 1
-                    stack.append(arg)
-                    current_dir = Dir(depth, stack[-1])
-                    parent[current_dir] = p
-                print(current_dir)
-            else:
-                ls[current_dir] += 1  # num times ls run in current dir
+    lines = f.readlines()
+
+lines = [line.strip() for line in lines]
+
+for line in lines:
+    if line.startswith("$ cd"):
+        *_, arg = line.split()
+        if arg == "/":
+            current_dir = root
+        elif arg == "..":
+            current_dir = current_dir.parent
         else:
-            # `line` is output from a previous command
-            # print(line)
-            field1, field2 = line.split()
-            if field1 == "dir":
-                # directory
-                pass
+            p = current_dir
+            current_dir = Dir(p.depth + 1, arg, p)
+        dirset.add(current_dir)
+    elif line.startswith("$ ls"):
+        ls[current_dir] += 1  # num times ls run in current dir
+        ls2[current_dir.name] += 1
+    else:
+        # `line` is output from a previous command
+        field1, field2 = line.split()
+        if field1 == "dir":
+            subdir = Dir(current_dir.depth + 1, field2, current_dir)
+            dirset2.add(subdir)
+            pass
+        else:
+            # file
+            file = File(int(field1), field2, current_dir)
+            fileset.add(file)
+            files[current_dir].add(field2)
+            if ls[current_dir] == 1:
+                size[current_dir] += int(field1)
             else:
-                # file
-                if ls[current_dir] == 1:
-                    size[current_dir] += int(field1)
+                print(f"ls'ed again: {current_dir}")
                
 
 ans = 0
 threshold = 100000
-# print(size)
 
-for dir in list(sorted(size, reverse=True)):
-    if size[dir] > threshold:
-        print(f"{dir}:  {size[dir]} > {threshold} --> ignore")
+fileless_dirs = 0
+for dir in dirset:
+    if dir not in size:
+        fileless_dirs += 1
+
+assert len(size) + fileless_dirs == len(dirset)
+
+
+for dir in list(sorted(dirset, reverse=True)):
+    if dir is not root:
+        print(dir.depth, dir.name, size[dir], dir.parent.depth, dir.parent.name, size[dir.parent])
+        if size[dir] > threshold:
+            print(f"{dir.name}:  {size[dir]} > {threshold} --> ignore")
+            pass
+        else:
+            print(f"{dir.name}:  {size[dir]} <= {threshold} --> include")
+            print(f"total is {ans} + {size[dir]} = {ans+size[dir]}")
+            ans += size[dir]
+        size[dir.parent] += size[dir]
+        size[dir] = 0
+        print(dir.depth, dir.name, size[dir], dir.parent.depth, dir.parent.name, size[dir.parent])
     else:
-        print(f"{dir}:  {size[dir]} <= {threshold} --> include")
-        print(f"total is {ans} + {size[dir]} = {ans+size[dir]}")
-        ans += size[dir]
-    try:
-        print(f"add to parent {parent[dir]}: {size[parent[dir]]} + {size[dir]} = {size[parent[dir]] + size[dir]}")
-        size[parent[dir]] += size[dir]
-        # size[dir] = 0
-    except KeyError:
-        print(f"{dir} has no parent")
+        # dir is root
+        if size[root] <= threshold:
+            ans += size[root]
 
-print(size)
 print(ans)
 
+# correct ans: 1325919
+# last guess: 1260326
+# last guess: 1517771
 # last guess: 1057868
 # last guess: 987658
-
-# Go to all leaves of tree
-# Since leaf has no subdirs, leaf size = direct size
-# Now treat leaf as file of parent dir
-
-# print(depths)
-
-# print(depths[full_depth])
-
-# for d in dirset:
-#     print(d)
-#     while parents[d]:
-#         d = parents[d]
-#         print(d)
-
-# print(parents)
-
-# for dir, subdirs in dirs.items():
-#     for subdir in subdirs:
-#         if len(dirs[subdir]) != 0:
-#             # subdir has children
-#             return get_children(dirs[child])
-#         else:
-#             # subdir has no children
-#             # check next subdir
-#             pass
-
-# for dir in direct_size.keys():  # For each dir that we saw...
-#     print(dir)
-#     if len(dirs[dir]) == 0:  # ... if that dir has no childen...
-#         sizes[dir] = direct_size[dir]  # ...its true size is its direct size
-#         # That dir can now be treated as a file.
-#         # Find the parent of that dir, remove the dir from the list
-#         # of children and increase the parent's direct size by 
-#         # the direct size of dir
-#         parent = parents[dir]
-#         print(f"dir: {dir},  parent: {parent}, dirs: {dirs}")
-#         direct_size[parent] += direct_size[dir]
-#         dirs[parent].remove(dir)
-#         # print(dirs)
